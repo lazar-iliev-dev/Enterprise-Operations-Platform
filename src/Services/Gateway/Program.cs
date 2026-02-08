16ee68e083bfe90d3bdd95fe66cd536adab3d8f1
lazar-iliev-dev <lazar.iliev@tutanota.de>
2026-02-08 11:50:18 +0100

    There are minor changes.

--- src/Services/Gateway/Program.cs
+++ src/Services/Gateway/Program.cs
@@ -1,7 +1,8 @@
 
-//using Aspire.Hosting;
+using Aspire.Hosting;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using System.Net.Security;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,15 @@ builder.AddServiceDefaults();
 // YARP Reverse Proxy
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+//ONLY FOR DEV: Ignore SSL errors when proxying to services with self-signed certs (e.g., in Docker)
+//builder.Services.AddReverseProxy()
+//  .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+//  .ConfigureHttpClient((context, handler) =>
+   // {
+        // Ignore certificate errors (only for Dev!)
+     //   handler.SslOptions.RemoteCertificateValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+//    });
 
 // JWT-Auth (zentral!)
 builder.Services.AddAuthentication().AddJwtBearer();
